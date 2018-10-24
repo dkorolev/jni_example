fcfc1b694142f6c44043f1734c50d42b6ca0e552
Dima <dmitry.korolev@gmail.com>
2018-10-24 14:30:21 -0700

    Reading input by lines.

--- Blah.java
+++ Blah.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Blah {
   static {
     System.loadLibrary("blah");
@@ -6,6 +8,11 @@ public class Blah {
   private native String doSomethingWithString(String string);
 
   public static void main(String[] args) {
-    System.out.println((new Blah()).doSomethingWithString("test"));
+    Blah blah = new Blah();
+    Scanner input = new Scanner(System.in);
+    System.out.println("Type some strings, and we'll route them through a JNI library. Ctrl+D to quit, of course.");
+    while (input.hasNextLine()) {
+      System.out.println(blah.doSomethingWithString(input.nextLine()));
+    }
   }
 }
